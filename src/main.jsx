b3ce3754afea4190f55024f18df3414a2adacb86
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-10-14 18:54:20 -0400

    props and whatnot

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,55 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
-const title = "Doing React without JSX";
-const desc = "rendering React into the dom"
+const Header = () => {
+    return (
+        <header>
+            <h1>Stuff List</h1>
+            <span className="total-items">Items: 1</span>
+        </header>
+    )
+}
 
-const element = (
-    <>
-        <h1>{title}</h1>
-        <p>{desc}</p>
-    </>
-)
+const Item = () => {
+    return (
+        <div className="item">
+            <button className="remove-item" />
+            <span className="item-name">Bread</span>
+            <Counter />            
+        </div>
+    )
+}
+
+const Counter = () => {
+    return (
+        <div className="quantity">
+            <span className = "qty-label">Qty:</span>
+            <button className="increment">+</button>
+            <button className="decrement">-</button>
+            <span className="quantity-amount">10</span>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="grocery-list">
+            <Header />
+            {/* the list */}
+            <Item />
+        </div>
+    )
+
+}
+
+
 
 
 const root = createRoot(document.getElementById("root"));
-root.render(element);
+root.render(
+
+    <StrictMode>
+        <App />
+    </StrictMode>    
+
+);
