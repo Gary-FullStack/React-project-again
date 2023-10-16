e3218336212effe1836408f27d3abef3c2729c5b
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-10-16 18:20:22 -0400

    more fuckin props

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,33 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 
-const Header = ({title, itemTotal}) => {
+const Header = (props) => {
     
     return (
         <header>
-            <h1>{title}</h1>
-            <span className="total-items">Items: {itemTotal}</span>
+            <h1>{props.title}</h1>
+            <span className="total-items">Items: {props.itemTotal}</span>
         </header>
     )
 }
 
-const Item = () => {
+const Item = (props) => {
     return (
         <div className="item">
             <button className="remove-item" />
-            <span className="item-name">Bread</span>
-            <Counter />            
+            <span className="item-name">{props.name}</span>
+            <Counter quantity={props.quantity}/>            
         </div>
     )
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="quantity">
             <span className = "qty-label">Qty:</span>
             <button className="increment">+</button>
             <button className="decrement">-</button>
-            <span className="quantity-amount">10</span>
+            <span className="quantity-amount">{props.quantity}</span>
         </div>
     )
 }
@@ -41,7 +41,11 @@ const App = () => {
 
 
             {/* the list */}
-            <Item />
+            <Item name="Onion" quantity={10} />
+            <Item name="naners" quantity={1} />
+            <Item name="taters" quantity={2} />
+            <Item name="glue" quantity={1} />
+            <Item name="snacks" quantity={99} />
         </div>
     )
 
