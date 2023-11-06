10357f3dd7021081cfa48d88e6c66a24a2c01649
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-05 19:25:07 -0500

    usestate and so on

--- src/main.jsx
+++ src/main.jsx
@@ -1,90 +1,104 @@
 import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 
-const items = [
-    {
-      name: "Apples",
-      quantity: 5,
-      id: 1
-    },
-    {
-      name: "Bananas",
-      quantity: 7,
-      id: 2
-    },
-    {
-      name: "Box of Pasta",
-      quantity: 1,
-        id: 3
-    },
-    {
-      name: "Cookies",
-      quantity: 12,
-        id: 4
-    }
-  ]
-
-const Header = (props) => {    
-    return (
-        <header>
-            <h1>{props.title}</h1>
-            <span className="total-items">Items: {props.itemTotal}</span>
-        </header>
-    )
-}
+const Header = (props) => {
+  return (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="total-items">Items: {props.itemTotal}</span>
+    </header>
+  );
+};
 
 const Item = (props) => {
-    return (
-        <div className="item">
-            <button className="remove-item" />
-            <span className="item-name">{props.name}</span>
-            <Counter quantity={props.quantity}/>            
-        </div>
-    )
-}
-
-const Counter = (props) => {
-    return (
-        <div className="quantity">
-            <span className = "qty-label">Qty:</span>
-            <button className="increment">+</button>
-            <button className="decrement">-</button>
-            <span className="quantity-amount">{props.quantity}</span>
-        </div>
-    )
-}
-
-const App = (props) => {
-    return (
-        <div className="grocery-list">
-            <Header 
-                title= "Grocery List" 
-                itemTotal={props.initialList.length} />
 
 
-            {/* the list */}
+  return (
+    <div className="item">
+      <button className="remove-item" onClick={() => props.removeItem(props.id)} />
+      <span className="item-name">{props.name}</span>
+      <Counter />
+    </div>
+  );
+};
 
-            {props.initialList.map(item => 
-                <Item 
-                    name={item.name}
-                    quantity={item.quantity}
-                    key={item.id} 
-                />
-            )}
+const Counter = () => {
+  const [quantity, setQuantity] = useState(0);
 
-        </div>
-    )
+  const incrementQuantity = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+    // if (props.name === "Apples") {
+    //     setQuantity(prevQuantity => prevQuantity + 1)
+    // }
+  };
 
-}
+  const decrementQuantity = () => {
+    if (quantity > 0) setQuantity((prevQuantity) => prevQuantity - 1);
+  };
 
+  return (
+    <div className="quantity">
+      <span className="qty-label">Qty:</span>
+      <button className="increment" onClick={incrementQuantity}>
+        +
+      </button>
+      <button className="decrement" onClick={decrementQuantity}>
+        -
+      </button>
+      <span className="quantity-amount">{quantity}</span>
+    </div>
+  );
+};
 
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      name: "Meaning of life",
+      id: 1,
+    },
+    {
+      name: "Sense of purpose",
+      id: 2,
+    },
+    {
+      name: "Will to live",
+      id: 3,
+    },
+    {
+      name: "pie",
+      id: 4,
+    },
+  ]);
 
+  const handleRemoveItem = (id) => {
+    setItems((prevItems) => prevItems.filter(i => i.id !== id));
+  }
+
+  return (
+    <div className="grocery-list">
+
+      <Header 
+        title="Grocery List" 
+        itemTotal={items.length}             
+      />
+     
+
+      {items.map(item => 
+        <Item 
+         name={item.name}
+         id={item.id} 
+         key={item.id} 
+         removeItem={handleRemoveItem}
+        />
+      )}
+
+    </div>
+  );
+};
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-
-    <StrictMode>
-        <App initialList={items}/>
-    </StrictMode>    
-
+  <StrictMode>
+    <App  />
+  </StrictMode>
 );
